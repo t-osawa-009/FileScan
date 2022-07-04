2ec8626db2dcb035696bb928b701a12f79312618
t-osawa-009 <da87435@gmail.com>
2022-07-04 19:00:20 +0900

    Add ignore_extension

--- Sources/FileScan/main.swift
+++ Sources/FileScan/main.swift
@@ -6,15 +6,26 @@ let main = command(
     Option<String?>("sourcePath", default: nil, description: "parse file paths"),
     Option<String?>("output_path", default: nil, description: "out put files"),
     Option<String?>("ignore_paths", default: nil, description: "ignore files"),
+    Option<String?>("ignore_extension", default: nil, description: "ignore extension"),
     Option<String>("verbose", default: "false", description: "Display the log"),
     Option<String?>("join", default: nil, description: "Merging the results of multiple paths")
-) { sourcePath, output_path, ignore_paths, verbose, join_path in
+) { sourcePath, output_path, ignore_paths, ignore_extension, verbose, join_path in
     let startDate = Date()
     let isVerbose = verbose.lowercased() == "true"
+    var ignorePaths: [String] = []
+    if let ignore_paths = ignore_paths {
+        ignorePaths = ignore_paths.split(separator: ",").map({ String($0) })
+    }
+    
+    var ignore_extensions: [String] = []
+    if let ignore_extension = ignore_extension {
+        ignore_extensions = ignore_extension.split(separator: ",").map({ String($0) })
+    }
+    
     var files: [File] = []
     if let sourcePath = sourcePath {
         do {
-            files.append(contentsOf: try findFiles(sourcePath: sourcePath, ignore_paths: ignore_paths))
+            files.append(contentsOf: try findFiles(sourcePath: sourcePath, ignore_paths: ignorePaths, ignore_extensions: ignore_extensions))
         } catch {
             print(error.localizedDescription)
         }
@@ -24,7 +35,7 @@ let main = command(
         let join_paths = join_path.split(separator: ",").map({ String($0) })
         join_paths.forEach { path in
             do {
-                files.append(contentsOf: try findFiles(sourcePath: path, ignore_paths: ignore_paths))
+                files.append(contentsOf: try findFiles(sourcePath: path, ignore_paths: ignorePaths, ignore_extensions: ignore_extensions))
             } catch {
                 print(error.localizedDescription)
             }
@@ -98,14 +109,21 @@ let main = command(
     }
 }
 
-func findFiles(sourcePath: String, ignore_paths: String?) throws -> [File] {
+func findFiles(sourcePath: String, ignore_paths: [String], ignore_extensions: [String]) throws -> [File] {
     let folder = try Folder(path: sourcePath)
     var files = find(folder: folder)
     files.append(contentsOf: folder.files)
-    if let ignore_paths = ignore_paths {
-        let ignorePaths = ignore_paths.split(separator: ",")
-        ignorePaths.forEach { path in
-            files = files.lazy.filter { !$0.path.contains(path)}
+    ignore_paths.forEach { path in
+        files = files.lazy.filter { !$0.path.contains(path)}
+    }
+    
+    ignore_extensions.forEach { ex in
+        files = files.lazy.filter {
+            if let fileExtension = $0.extension {
+                return fileExtension.contains(ex)
+            } else {
+                return false
+            }
         }
     }
     
